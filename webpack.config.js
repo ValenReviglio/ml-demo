64b63dd3f1a8f47d52c21e4b67233cad27087fa3
Valentin Reviglio <valentin.reviglio@mercadolibre.com>
2019-11-12 11:02:33 -0300

    Webpack config was changed

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
     },
   },
   output: {
+    libraryTarget: 'commonjs2',
+    library: '[name]',
     path: path.join(path.resolve('.'), './build'),
     filename: '[name].js',
   },
